9b2f03476a1197c4b78f9108716de700cd123383
Maximilian Fornacon <mfornacon@pop-os.localdomain>
2018-12-12 00:23:52 +0100

    #54 Clean code

--- code/src/main/python/controller/start_controller.py
+++ code/src/main/python/controller/start_controller.py
@@ -2,7 +2,4 @@ class StartController:
     def __init__(self, view):
         self.start_view = view
         self.start_view.show()
-        self.start_view.MainWindow.start_frame.new_project_button.clicked.connect(self.test_button)
 
-    def test_button(self):
-        print("dsfsdf")
--- code/src/main/python/main.py
+++ code/src/main/python/main.py
@@ -1,6 +1,4 @@
 from fbs_runtime.application_context import ApplicationContext
-from controller import VideoEditorController
-from view import VideoEditorView
 from controller import MainController
 from view import StartView
 
@@ -9,8 +7,6 @@ import sys
 
 class AppContext(ApplicationContext):
     def run(self):
-        # video_editor_view = VideoEditorView()
-        # video_editor_controller = VideoEditorController(video_editor_view)
         start_view = StartView()
         __main_controller = MainController(start_view)
         __main_controller.start()
