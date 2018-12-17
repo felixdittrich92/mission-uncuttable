603fb526064728b02fcea205d97ccc6fc690231b
Felix Dittrich <felixdittrich92@gmail.com>
2018-12-17 22:48:01 +0100

    #MU-55 Convert PDF in JPG

---
+++ code/src/main/python/controller/foil_controller.py
@@ -0,0 +1,10 @@
+from wand.image import Image as wa
+
+def convert():
+    pdf = wa(filename = "XXX.pdf", resolution = 720)
+    pdfImage = pdf.convert("jpeg")
+    i = 1
+    for img in pdfImage.sequence:
+        page = wa(image=img)
+        page.save(filename = str(i) + ".jpg")
+        i += 1
