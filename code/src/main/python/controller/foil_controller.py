c1f79066041411866d7a6b37a0c6592a75f0f5d3
Felix Dittrich <felixdittrich92@gmail.com>
2019-01-05 20:42:33 +0100

    foil

--- code/src/main/python/controller/foil_controller.py
+++ code/src/main/python/controller/foil_controller.py
@@ -1,10 +1,11 @@
 from wand.image import Image as wa
 
+"""currently save as a own image"""
 def convert():
     pdf = wa(filename = "XXX.pdf", resolution = 720)
     pdfImage = pdf.convert("jpeg")
     i = 1
     for img in pdfImage.sequence:
-        page = wa(image=img)
-        page.save(filename = str(i) + ".jpg")
+        page = wa(image = img)
+        page.save(filename = str(i) + ".jpg") 
         i += 1
