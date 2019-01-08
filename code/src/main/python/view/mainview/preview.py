82e78732849bfaaca66c2d1315f1ea3570b1178d
alex <alexanderbonin@gmx.de>
2019-01-08 13:02:53 +0100

    first icon!!!

--- code/src/main/python/view/mainview/preview.py
+++ code/src/main/python/view/mainview/preview.py
@@ -1,9 +1,19 @@
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget, QPushButton
 from PyQt5 import uic
+from PyQt5 import QtGui
+from PyQt5.QtGui import QIcon 
+#from PyQt5 import QObject
 import os
 
 class PreviewView(QWidget):
     def __init__(self):
         super(PreviewView, self).__init__()
         path = os.path.abspath('src/main/python/view/mainview')
+        path2 = os.path.abspath('src/main/icons/preview_buttons/svg/')
+
         uic.loadUi(os.path.join(path, 'form.ui'), self)
+        playButton = self.findChild(QPushButton, "playButton")
+        icon = QtGui.QPixmap(os.path.join(path2,'002-play-button.svg'))
+        playButton.setIcon(QIcon(icon))
+
+        
