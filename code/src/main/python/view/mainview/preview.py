99df2e9e92471d8a492403f1513f8e5bf05ce2ec
alex <alexanderbonin@gmx.de>
2019-01-08 17:50:58 +0100

    video running

--- code/src/main/python/view/mainview/preview.py
+++ code/src/main/python/view/mainview/preview.py
@@ -5,6 +5,17 @@ from PyQt5.QtGui import QIcon
 #from PyQt5 import QObject
 import os
 
+from PyQt5.QtWidgets import QWidget, QLabel, QApplication, QMainWindow
+from PyQt5.QtGui import QIcon, QPixmap, QImage
+from PyQt5.QtCore import QThread, QObject, pyqtSignal, pyqtSlot
+from PyQt5.QtCore import Qt
+import sys
+
+import cv2 
+import numpy
+
+
+
 class PreviewView(QWidget):
     def __init__(self):
         super(PreviewView, self).__init__()
@@ -30,6 +41,30 @@ class PreviewView(QWidget):
         lastframeButton.setIcon(QIcon(iconlastframe))
         backButton.setIcon(QIcon(iconback))
         forwardButton.setIcon(QIcon(iconforward))
+
+        self.label = QLabel(self)
+        self.label.move(280, 120)
+        self.label.resize(640, 480)
+        th = Thread(self)
+        th.changePixmap.connect(self.setImage)
+        th.start()
+
+    @pyqtSlot(QImage)
+    def setImage(self, image):
+       self.label.setPixmap(QPixmap.fromImage(image))
+
+class Thread(QThread):
+    changePixmap = pyqtSignal(QImage)
+
+    def run(self):
+        cap = cv2.VideoCapture('./video.mp4')
+        while True:
+            ret, frame = cap.read()
+            if ret:
+                rgbImage = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                convertToQtFormat = QImage(rgbImage.data, rgbImage.shape[1], rgbImage.shape[0], QImage.Format_RGB888)
+                p = convertToQtFormat.scaled(640, 480, Qt.KeepAspectRatio)
+                self.changePixmap.emit(p)
 
 
         
