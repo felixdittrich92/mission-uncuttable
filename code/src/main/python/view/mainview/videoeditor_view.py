935b03d26aa4d828f8b2e9004fd3f0e22334ac2e
markus_leupold <markus.leupold@stud.htwk-leipzig.de>
2019-01-20 21:18:37 +0100

    #64 Add the TimelineView to the main window
    Add a function which replaces the placeholder 'bottomFrame' of the main
    window by an instance of TimelineView. This function gets called after
    the main layout of the window has been loaded.

--- code/src/main/python/view/mainview/videoeditor_view.py
+++ code/src/main/python/view/mainview/videoeditor_view.py
@@ -1,6 +1,9 @@
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QSplitter
+from PyQt5.QtCore import QObject
 from PyQt5 import uic
 import os
+
+from view.timeline.timelineview.timeline_view import TimelineView
 
 
 class VideoEditorView(QMainWindow):
@@ -10,6 +13,20 @@ class VideoEditorView(QMainWindow):
         super(VideoEditorView, self).__init__()
         path = os.path.abspath('src/main/python/view/mainview')
         uic.loadUi(path + '/main_window.ui', self)
+        self.load_timeline_widget()
+
+    def load_timeline_widget(self):
+        """
+        Replaces the 'bottomFrame'-named QObject with a new instance of
+        TimelineView. The widget doesn't have any real content yet after
+        execution of this method.
+        """
+        splitter = self.findChild(QObject, 'horizontalSplitter')
+        bottom_frame = self.findChild(QObject, 'bottomFrame')
+        i = splitter.indexOf(bottom_frame)
+        timeline_view = TimelineView()
+        splitter.replaceWidget(i, timeline_view)
+        timeline_view.show()
 
     def show(self):
         """Starts the video-editor-window maximized."""
