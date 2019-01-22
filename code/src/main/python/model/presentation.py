8e5df13043a9e7231c405b68ed29f2242e675f44
Felix Dittrich <felixdittrich92@gmail.com>
2019-01-22 10:03:28 +0100

    #55 model / method update

--- code/src/main/python/model/presentation.py
+++ code/src/main/python/model/presentation.py
@@ -1,11 +1,24 @@
 from wand.image import Image as wa
 
-"""currently save as a own image"""
-def convert():
-    pdf = wa(filename = "XXX.pdf", resolution = 720)
-    pdfImage = pdf.convert("jpeg")
-    i = 1
-    for img in pdfImage.sequence:
-        page = wa(image = img)
-        page.save(filename = str(i) + ".jpg") 
-        i += 1
+class Presentation:
+        """A class used to divide a slide into images"""
+
+        def __init__(self, path, filename):
+                """Constructor of the class"""
+
+                print('Image erstellt')
+                pass
+
+        def convert(self, path, filename):
+                """Method that divides a slide into individual images and stores them in the working memory"""
+
+                pdf = wa(file = path + filename, resolution = 720)
+                pdf_image = pdf.convert("jpeg")
+                i = 1
+                for img in pdf_image.sequence:
+                        page = wa(image = img)
+                        filename = (str(1) + ".jpg")
+                        presentation = Presentation(page, filename)
+                        i += 1
+                
+
