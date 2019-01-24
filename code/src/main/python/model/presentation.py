27711be51105096f953eccb3b82fe2397744148d
Felix Dittrich <felixdittrich92@gmail.com>
2019-01-24 09:14:57 +0100

    #55 finished

--- code/src/main/python/model/presentation.py
+++ code/src/main/python/model/presentation.py
@@ -1,18 +1,21 @@
 from wand.image import Image as wa
+from slide import Slide
+
 
 class Presentation:
-        
-        def __init__(self, path, filename):
-                print('Image erstellt')
-                pass
+    """Class that divides a PDF into individual images and converts them to jpg"""
+    def __init__(self):
+        """Constructor of the class"""
+        print('Image erstellt')
+        pass
 
-        def convert(self, path, filename):
-                pdf = wa(file = path + filename, resolution = 720)
-                pdf_image = pdf.convert("jpeg")
-                i = 1
-                for img in pdf_image.sequence:
-                        page = wa(image = img)
-                        #filename = (str(1) + ".jpg")
-                        slide[i] = Slide(i, page)
-                        i += 1
-
+    def convert(self, path, filename):
+        """a method that takes a path and a PDF file, converts them to JPG, and then saves the individual images"""
+        pdf = wa(file=path + filename, resolution=300)
+        pdf_images = pdf.convert("jpeg")
+        page_number = 1
+        for img in pdf_images.sequence:
+            page = wa(image=img)
+            slide = []
+            slide.append(Slide(page_number, page))
+            page_number += 1
