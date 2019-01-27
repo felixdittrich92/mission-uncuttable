506228248c2642bfd84e1e0a31ce546346d99361
Clemens Zwinzscher <clemens.zwinzscher@stud.htwk-leipzig.de>
2019-01-27 13:03:14 +0100

    #64 add some trackviews to see how it looks like

--- code/src/main/python/view/timeline/timelineview/timeline_view.py
+++ code/src/main/python/view/timeline/timelineview/timeline_view.py
@@ -1,9 +1,11 @@
-from PyQt5.QtWidgets import QFrame, QLabel
+from PyQt5.QtWidgets import QFrame, QLabel, QPushButton
 from PyQt5 import uic
 from PyQt5.QtCore import QObject
 import os
 
 from .timeline_scroll_area import TimelineScrollArea
+from view.timeline.trackview import TrackView
+from view.timeline.timeableview import TimeableView
 
 
 class TimelineView(QFrame):
@@ -25,8 +27,43 @@ class TimelineView(QFrame):
         path = os.path.abspath('src/main/python/view/timeline/timelineview')
         uic.loadUi(path + '/timeline_view.ui', self)
         self.layout().addWidget(TimelineScrollArea())
+        self.track_frame = self.findChild(QFrame, "track_frame")
+        self.track_button_frame = self.findChild(QFrame, "track_button_frame")
 
+        self.__show_tracks()
         self.__show_debug_info_on_gui()
+
+    def __show_tracks(self):
+        """shows some tracks with timeables to see if everything works"""
+        tr1 = TrackView(2000, 100)
+        tr1.add_timeable(TimeableView("timeable1", 200, 100, 0))
+        tr1.add_timeable(TimeableView("timeable2", 400, 100, 300))
+        self.track_frame.add_track(tr1)
+        btn1 = QPushButton("Track 1")
+        btn1.setFixedSize(100, 100)
+        self.track_button_frame.add_button(btn1)
+
+        tr2 = TrackView(2000, 100)
+        tr2.add_timeable(TimeableView("timeable3", 200, 100, 150))
+        self.track_frame.add_track(tr2)
+        btn2 = QPushButton("Track 2")
+        btn2.setFixedSize(100, 100)
+        self.track_button_frame.add_button(btn2)
+
+        tr3 = TrackView(2000, 100)
+        tr3.add_timeable(TimeableView("timeable4", 300, 100, 700))
+        self.track_frame.add_track(tr3)
+        btn3 = QPushButton("Track 3")
+        btn3.setFixedSize(100, 100)
+        self.track_button_frame.add_button(btn3)
+
+        tr4 = TrackView(2000, 100)
+        tr4.add_timeable(TimeableView("timeable5", 300, 100, 400))
+        tr4.add_timeable(TimeableView("timeable6", 100, 100, 0))
+        self.track_frame.add_track(tr4)
+        btn4 = QPushButton("Track 4")
+        btn4.setFixedSize(100, 100)
+        self.track_button_frame.add_button(btn4)
 
     def __show_debug_info_on_gui(self):
         """
@@ -39,5 +76,3 @@ class TimelineView(QFrame):
             0, QLabel(toolbar_frame.objectName())
         )
         self.setStyleSheet('background-color: yellow')
-
-
