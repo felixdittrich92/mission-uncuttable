6e2f75c11a458f0db0d3137b56530c5dbed13e26
Felix Dittrich <felixdittrich92@gmail.com>
2019-03-19 15:14:31 +0100

    #56 color check works

--- code/src/main/python/model/__init__.py
+++ code/src/main/python/model/__init__.py
@@ -1,6 +1,6 @@
-#from .video import Video
 #from .history import History
 #from .project import Project
 #from .operation import Operation
 from .presentation import Presentation
 #from .video_in_slide import VideoInSlide
+#from .video import Video
--- code/src/main/python/model/presentation.py
+++ code/src/main/python/model/presentation.py
@@ -1,4 +1,8 @@
 from pdf2image import convert_from_path
+import os
+import numpy
+import cv2
+from PIL import Image
 from pathlib import Path
 
 
@@ -9,12 +13,45 @@ class Presentation:
         """Constructor of the class"""
         pass
 
-    def convert(self, path, filename, new_project_path, new_project_name):
-        """a function that creates a new project folder split the pdf to pictures and save them in the new folder"""
+    def convert(self, file_path, filename, new_project_path, new_project_name):
+        """a method that creates a new project folder takes a pdf-file and split it into pictures which will save in the new folder"""
         folder = Path(new_project_path, new_project_name)
         folder.mkdir(exist_ok=True) 
-        input_file = Path(path, filename)
-        pages = convert_from_path(str(input_file), 300)
+        input_file = Path(file_path, filename)
+        pages = convert_from_path(str(input_file), 250) #dont touch this number !!
+        files = []
+
         for page_number, page in enumerate(pages, start=1):
             target = folder / f"{page_number:03d}.jpg"
             page.save(str(target),  'JPEG')
+
+        for file in os.listdir(folder):
+            files.append(file)
+
+        files.sort()
+
+
+    def check_color(self, file_path, filename, left_pixel, upper_pixel, right_pixel, lower_pixel):
+        """a method that checks if the place in the picture for video is free"""
+        """description: left_pixel and upper_pixel are the (x,y) points for the video place like (0,0) in a coordinate system, right_pixel and lower_pixel are the coordinates of the lower right corner"""
+        """@return: true / false"""
+        input_file = Path(file_path, filename)
+        img = Image.open(input_file)
+        width, height = img.size  #1260x945
+        #box = (left_pixel, upper_pixel, right_pixel, lower_pixel)
+        imgload = img.load()
+        x = left_pixel
+        y = upper_pixel
+        window_width = right_pixel - left_pixel   #width of the videoplace
+        
+        while True:
+            point = imgload[x, y]
+            if point == (255, 255, 255):
+                x += 1
+                if x == width:
+                    x -= window_width
+                    y += 1
+                elif x == right_pixel and y == lower_pixel:  
+                        return True
+            else:
+                return False
