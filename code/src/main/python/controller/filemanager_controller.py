37376d9c1d53d60f5300cec8efed1f0ad1933f20
Felix Dittrich <felixdittrich92@gmail.com>
2019-04-22 20:32:27 +0200

    #160 show files and tooltips in filemanager

--- code/src/main/python/controller/filemanager_controller.py
+++ code/src/main/python/controller/filemanager_controller.py
@@ -1,8 +1,11 @@
 import sys
 
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QFileDialog, QWidget
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QIcon, QPixmap, QImage
+from PyQt5.QtWidgets import QApplication, QFileDialog, QWidget, QListWidget, QVBoxLayout, QLabel, QPushButton, QListWidgetItem, QGridLayout
 from PyQt5.QtCore import QObject
+from PIL import Image, ImageQt
 
 from config import Resources
 
@@ -12,11 +15,13 @@ class Filemanager(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         uic.loadUi(Resources.get_instance().files.filemanager, self)
-        self.pushButton_3 = self.findChild(QObject,'pushButton_3')
-        self.pushButton_2 = self.findChild(QObject,'pushButton_2')
+        self.pickButton = self.findChild(QObject,'pushButton_2')
+        self.clearButton = self.findChild(QObject,'pushButton_3')
         self.listWidget = self.findChild(QObject,'listWidget')
-        self.pushButton_2.clicked.connect(self.pickFileNames)
-        self.pushButton_3.clicked.connect(self.clearFileNames)
+        #self.listWidget.setSizeHint(QSize(0,200))
+
+        self.pickButton.clicked.connect(self.pickFileNames)
+        self.clearButton.clicked.connect(self.clearFileNames)
 
     def pickFileNames(self):
         fileNames, _ = QFileDialog.getOpenFileNames(
@@ -30,12 +35,33 @@ class Filemanager(QWidget):
                 'Movie Files (*.mp4)'
             )
         )
+
         self.addFileNames(fileNames)
 
     def addFileNames(self, fileNames):
-        self.listWidget.addItems(fileNames)
+        last_element = fileNames[-1]
+
+        if last_element.endswith(('.jpg','.JPEG','.JPG')):
+            picture = Image.open(last_element)
+            picture.thumbnail(((200,200)), Image.ANTIALIAS)
+            icon = QIcon(QPixmap.fromImage(ImageQt.ImageQt(picture)))
+            item = QListWidgetItem(last_element[:20] + "...", self.listWidget)
+            item.setStatusTip(last_element)
+            item.setIcon(icon)
+        elif last_element.endswith('.mp4'):
+            picture = Image.open(last_element)
+            picture.thumbnail(((400,400)), Image.ANTIALIAS)
+            icon = QIcon(QPixmap.fromImage(ImageQt.ImageQt(picture)))
+            item = QListWidgetItem(last_element[:20] + "...", self.listWidget)
+            item.setStatusTip(last_element)
+            item.setIcon(icon)
+        elif last_element.endswith('.mp3'):
+            pass # TO DO
+
+
 
     def clearFileNames(self):
+
         self.listWidget.clear()
 
 
@@ -48,4 +74,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
