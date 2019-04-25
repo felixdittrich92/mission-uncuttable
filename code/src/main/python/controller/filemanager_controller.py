4e237001c2206800fa8b114c512e4037df6ae429
Felix <you@example.com>
2019-04-25 11:48:56 +0200

    #161 add drop & #160 add hover

--- code/src/main/python/controller/filemanager_controller.py
+++ code/src/main/python/controller/filemanager_controller.py
@@ -8,6 +8,10 @@ from PyQt5.QtGui import QIcon, QPixmap, QImage
 from PyQt5.QtWidgets import QApplication, QFileDialog, QWidget, QListWidget, QVBoxLayout, QLabel, QPushButton, QListWidgetItem, QGridLayout
 from PyQt5.QtCore import QObject, QSize
 from PIL import Image, ImageQt
+from pathlib import Path
+from pygame import mixer
+import numpy as np
+import cv2
 
 from config import Resources
 
@@ -21,7 +25,8 @@ class Filemanager(QWidget):
         self.clearButton = self.findChild(QObject,'pushButton_3')
         self.listWidget = self.findChild(QObject,'listWidget')
         self.listWidget.setDragEnabled(True)
-        self.listWidget.setAcceptDrops(True)
+        self.listWidget.setAcceptDrops(False)
+        self.listWidget.setMouseTracking(True)
         self.listWidget.setIconSize(QSize(100,100))
 
         self.pickButton.clicked.connect(self.pickFileNames)
@@ -51,25 +56,29 @@ class Filemanager(QWidget):
             icon = QIcon(QPixmap.fromImage(ImageQt.ImageQt(picture)))
             item = QListWidgetItem(os.path.basename(last_element)[:20], self.listWidget)
             item.setToolTip(last_element)
-            item.setStatusTip(last_element)
+            self.listWidget.setStatusTip(last_element)
             item.setIcon(icon)
         elif last_element.endswith('.mp4'):
-            filename = '/home/felix/Schreibtisch/softwareprojekt/mission-uncuttable/code/src/main/resources/base/images/files/mp4logo.jpg'
-            #ToDo
-            picture = Image.open(filename)
+            path = "/home/felix/Schreibtisch/softwareprojekt/mission-uncuttable/code/src/main/resources/base/images/files/"
+            filename = "mp4logo.jpg"
+            path_to_file = Path(path, filename)
+            #ToDo path muss variabel über Einstellungen sein
+            picture = Image.open(path_to_file)
             icon = QIcon(QPixmap.fromImage(ImageQt.ImageQt(picture)))
             item = QListWidgetItem(os.path.basename(last_element)[:20], self.listWidget)
             item.setToolTip(last_element)
-            item.setStatusTip(last_element)
+            self.listWidget.setStatusTip(last_element)
             item.setIcon(icon)
         elif last_element.endswith('.mp3'):
-            filename = "/home/felix/Schreibtisch/softwareprojekt/mission-uncuttable/code/src/main/resources/base/images/files/mp3logo.jpg"
-            #ToDo
-            picture = Image.open(filename)
+            path = "/home/felix/Schreibtisch/softwareprojekt/mission-uncuttable/code/src/main/resources/base/images/files/"
+            filename = "mp3logo.jpg"
+            path_to_file = Path(path, filename)
+            #ToDo path muss variabel über Einstellungen sein
+            picture = Image.open(path_to_file)
             icon = QIcon(QPixmap.fromImage(ImageQt.ImageQt(picture)))
             item = QListWidgetItem(os.path.basename(last_element)[:20], self.listWidget)
             item.setToolTip(last_element)
-            item.setStatusTip(last_element)
+            self.listWidget.setStatusTip(last_element)
             item.setIcon(icon)
         else:
             print("The datatype is not supported")
@@ -79,8 +88,35 @@ class Filemanager(QWidget):
 
         self.listWidget.clear()
 
+    def enterEvent(self, event):
+        file_path = self.listWidget.statusTip()
+        
+        if file_path.endswith('.mp4'):
+            cap = cv2.VideoCapture(str(file_path))
+
+            while(cap.isOpened()):
+                ret,frame = cap.read()
+                #frame = cv2.resize(frame, (200,200))
+                cv2.imshow('frame', frame)
+
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+            
+            cap.release()
+            cv2.destroyAllWindows()
+
+        elif file_path.endswith('.mp3'):
+            mixer.init()
+            mixer.music.load(file_path) #pygame.error: Unrecognized music format
+            mixer.music.play()
+        
+        else:
+            pass
+
+
     def dragEnterEvent(self, event):
         if event.mimeData().hasUrls():
+            print(event.mimeData().urls())
             event.accept()
         else:
             event.ignore()
@@ -94,10 +130,28 @@ class Filemanager(QWidget):
     
     def dropEvent(self, event):
         if event.mimeData().hasUrls():
-            event.setDropAction(Qt.CopyAction) #abfrage jpg/mp4/mp3...dann addClip ?
+            event.setDropAction(Qt.CopyAction) 
             event.accept()
+            l  = []
+            for url in event.mimeData().urls():
+                l.append(str(url.toLocalFile()))
+            self.emit(SIGNAL("dropped"),l)
         else:
             event.ignore()  
+    
+    #self.connect(self.timeview, SIGNAL("dropped"), self.fileDropped)
+
+    #def fileDropped(self, l):
+    #    file = l[-1]
+
+    #    if file.endswith(('.jpg','.JPEG', '.jpeg', '.JPG','.png', '.PNG')):
+    #        addClip()
+    #    elif file.endswith('.mp4'):
+    #        addClip()
+    #    elif file.endswith('.mp3'):
+    #        addClip()
+    #    else:
+    #        pass
 
 
 def main():
