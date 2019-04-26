f576dd4f0f484091f96b80bed5618877fbce07d3
tjeske <tim.jeske@stud.htwk-leipzig.de>
2019-04-26 18:23:02 +0200

    Fix documentation
    Fix Doc Strings that where at wrong positions.

--- code/src/main/python/model/project/history.py
+++ code/src/main/python/model/project/history.py
@@ -1,9 +1,9 @@
 from .operation import Operation
 
-"""
-Represents the history of a project and manages operations.
-"""
 class History:
+    """
+    Represents the history of a project and manages operations.
+    """
     def __init__(self, operations = []):
         self.operations = operations
         self.__last_operation = len(operations) - 1
@@ -11,10 +11,10 @@ class History:
     def apply_history(self):
         pass
 
-    """
-    Function that undos the last operation in the history.
-    """
     def undo_last_operation(self):
+        """
+        Function that undos the last operation in the history.
+        """
         if (self.__last_operation > -1):
             self.operations[self.__last_operation].undo()
             self.__last_operation -= 1
@@ -22,22 +22,22 @@ class History:
             raise Exception("No operation to undo in history. History is empty.")
         
 
-    """
-    Function that redos the last undone operation in the history.
-    """
     def redo_last_operation(self):
+        """
+        Function that redos the last undone operation in the history.
+        """
         if (self.__last_operation + 1 < len(self.operations)):
             self.operations[self.__last_operation + 1].do()
             self.__last_operation += 1
         else:
             raise Exception("No operation to redo in history.")
 
-    """
-    Executes a given operation and adds it to the history.
-    @type   operation: Operation
-    @param  operation: An operation that is executed.
-    """
     def do_operation(self, operation):
+        """
+        Executes a given operation and adds it to the history.
+        @type   operation: Operation
+        @param  operation: An operation that is executed.
+        """
         # Add operation to history
         if(len(self.operations) > self.__last_operation + 1):
             # Other operation is overriden
@@ -52,16 +52,16 @@ class History:
         operation.do()
         print(self.__last_operation)
 
-    """
-    Function that returns the Number of operations that can be undone
-    """
     def get_num_operations(self):
+        """
+        Function that returns the Number of operations that can be undone
+        """
         return self.__last_operation + 1
 
-    """
-    Function that returns the number of operations that can be redone.
-    @rtype Number
-    @return Number of operations that can be redone.
-    """
     def get_num_undone_operations(self):
+        """
+        Function that returns the number of operations that can be redone.
+        @rtype Number
+        @return Number of operations that can be redone.
+        """
         return len(self.operations) - self.__last_operation
