febef3258c0af1ffa2db0116e865cb1961ca4036
tjeske <tim.jeske@stud.htwk-leipzig.de>
2019-04-26 18:02:16 +0200

    Implement history
    Implement basic management of operations in the history. Add methods for undo, redo and do_operation.

--- code/src/main/python/model/project/history.py
+++ code/src/main/python/model/project/history.py
@@ -1,20 +1,67 @@
-from .operation import Operation
+from operation import Operation
 
+"""
+Represents the history of a project and manages operations.
+"""
 class History:
     def __init__(self, operations = []):
-        if(len(operations) > 0):
-            self.operations = operations
-        else:
-            self.operations = []
-        
+        self.operations = operations
+        self.__last_operation = len(operations) - 1
+    
     def apply_history(self):
         pass
 
-    def undo(self):
-        pass
+    """
+    Function that undos the last operation in the history.
+    """
+    def undo_last_operation(self):
+        if (self.__last_operation > -1):
+            self.operations[self.__last_operation].undo()
+            self.__last_operation -= 1
+        else:
+            raise Exception("No operation to undo in history. History is empty.")
+        
 
-    def redo(self):
-        pass
+    """
+    Function that redos the last undone operation in the history.
+    """
+    def redo_last_operation(self):
+        if (self.__last_operation + 1 < len(self.operations)):
+            self.operations[self.__last_operation + 1].do()
+            self.__last_operation += 1
+        else:
+            raise Exception("No operation to redo in history.")
 
-    def do_operation(self, operations):
-        pass    
+    """
+    Executes a given operation and adds it to the history.
+    @type   operation: Operation
+    @param  operation: An operation that is executed.
+    """
+    def do_operation(self, operation):
+        # Add operation to history
+        if(len(self.operations) > self.__last_operation + 1):
+            # Other operation is overriden
+            self.operations[self.__last_operation + 1] = operation
+            # Remove undone operations
+            self.operations = self.operations[0:(self.__last_operation + 2)]
+        else:
+            # Operation is added at the end
+            self.operations.append(operation)
+        self.__last_operation += 1
+        # Execute operation
+        operation.do()
+        print(self.__last_operation)
+
+    """
+    Function that returns the Number of operations that can be undone
+    """
+    def get_num_operations(self):
+        return self.__last_operation + 1
+
+    """
+    Function that returns the number of operations that can be redone.
+    @rtype Number
+    @return Number of operations that can be redone.
+    """
+    def get_num_undone_operations(self):
+        return len(self.operations) - self.__last_operation
