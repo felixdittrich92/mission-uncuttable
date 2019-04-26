85f512929bcf82e03e8a56b143e4f59614a029f8
Clemens Zwinzscher <clemens.zwinzscher@stud.htwk-leipzig.de>
2019-04-26 13:51:44 +0200

    add export option

--- code/src/main/python/controller/videoeditor_controller.py
+++ code/src/main/python/controller/videoeditor_controller.py
@@ -1,8 +1,11 @@
+import openshot
+
 from .settings_controller import SettingsController
 from view.settingsview import SettingsView
 from .projectsettings_controller import ProjectSettingsController
 from view.settingsview import ProjectSettingsView
 
+from model.project.timeline import TimelineModel
 
 
 class VideoEditorController:
@@ -15,6 +18,7 @@ class VideoEditorController:
         self.__video_editor_view = view
         self.__video_editor_view.action_settings.triggered.connect(self.__start_settings_controller)
         self.__video_editor_view.action_projectsettings.triggered.connect(self.__start_projectsettings_controller)
+        self.__video_editor_view.actionExport.triggered.connect(self.__export)
 
     def __show_view(self):
         """Calls show() of 'VideoEditorView'."""
@@ -39,3 +43,24 @@ class VideoEditorController:
         projectsettings_view = ProjectSettingsView()
         self.__projectsettings_controller = ProjectSettingsController(projectsettings_view)
         self.__projectsettings_controller.start()
+
+    def __export(self):
+        """exports the timeline"""
+        tm = TimelineModel.get_instance()
+        t = tm.timeline
+
+        # testing data
+        audio_options = [True, "aac", 48000, 2, 3, 96000]
+        video_options = [True, "libx264", t.info.fps, t.info.width,
+                         t.info.height, openshot.Fraction(1, 1), False, False, 384000]
+
+        # get the number of the last frame
+        last_frame = 0
+        for c in t.Clips():
+            clip_last_frame = c.Position() + c.Duration()
+            if clip_last_frame > last_frame:
+                last_frame = clip_last_frame
+
+        last_frame = round(last_frame * t.info.fps.ToFloat()) + 1
+
+        tm.export("exported.mp4", audio_options, video_options, last_frame)
