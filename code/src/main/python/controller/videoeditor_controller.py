f19b75a53d5f1ccd53bf81cd6e017a0d7b7ed4ac
Clemens Zwinzscher <clemens.zwinzscher@stud.htwk-leipzig.de>
2019-05-01 17:01:26 +0200

    open exportview from videoeditor_controller

--- code/src/main/python/controller/videoeditor_controller.py
+++ code/src/main/python/controller/videoeditor_controller.py
@@ -1,11 +1,8 @@
-import openshot
-
 from .settings_controller import SettingsController
 from view.settingsview import SettingsView
 from .projectsettings_controller import ProjectSettingsController
 from view.settingsview import ProjectSettingsView
-
-from model.project.timeline import TimelineModel
+from view.exportview import ExportView
 
 
 class VideoEditorController:
@@ -16,9 +13,12 @@ class VideoEditorController:
     """
     def __init__(self, view):
         self.__video_editor_view = view
-        self.__video_editor_view.action_settings.triggered.connect(self.__start_settings_controller)
-        self.__video_editor_view.action_projectsettings.triggered.connect(self.__start_projectsettings_controller)
-        self.__video_editor_view.actionExport.triggered.connect(self.__export)
+        self.__video_editor_view.action_settings.triggered.connect(
+            self.__start_settings_controller)
+        self.__video_editor_view.action_projectsettings.triggered.connect(
+            self.__start_projectsettings_controller)
+        self.__video_editor_view.actionExport.triggered.connect(
+            self.__start_export_controller)
 
     def __show_view(self):
         """Calls show() of 'VideoEditorView'."""
@@ -33,34 +33,18 @@ class VideoEditorController:
         self.__video_editor_view.close()
 
     def __start_settings_controller(self):
-        "Opens the settings window"
+        """Opens the settings window"""
         settings_view = SettingsView()
         self.__settings_controller = SettingsController(settings_view)
         self.__settings_controller.start()
 
     def __start_projectsettings_controller(self):
-        "Opens the projectsettings window"
+        """Opens the projectsettings window"""
         projectsettings_view = ProjectSettingsView()
         self.__projectsettings_controller = ProjectSettingsController(projectsettings_view)
         self.__projectsettings_controller.start()
 
-    def __export(self):
-        """exports the timeline"""
-        tm = TimelineModel.get_instance()
-        t = tm.timeline
-
-        # testing data
-        audio_options = [True, "aac", 48000, 2, 3, 96000]
-        video_options = [True, "libx264", t.info.fps, t.info.width,
-                         t.info.height, openshot.Fraction(1, 1), False, False, 384000]
-
-        # get the number of the last frame
-        last_frame = 0
-        for c in t.Clips():
-            clip_last_frame = c.Position() + c.Duration()
-            if clip_last_frame > last_frame:
-                last_frame = clip_last_frame
-
-        last_frame = round(last_frame * t.info.fps.ToFloat()) + 1
-
-        tm.export("exported.mp4", audio_options, video_options, last_frame)
+    def __start_export_controller(self):
+        """shows the export view"""
+        export_view = ExportView()
+        export_view.show_window()
