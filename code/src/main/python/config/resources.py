a34f17a9da9c86a0892a73cf567c23b71fd333b5
Clemens Zwinzscher <clemens.zwinzscher@stud.htwk-leipzig.de>
2019-05-01 17:00:20 +0200

    add export.ui to resources

--- code/src/main/python/config/resources.py
+++ code/src/main/python/config/resources.py
@@ -1,4 +1,3 @@
-import os
 files = {
     "startview": "ui/start_view.ui",
     "filemanager": "ui/filemanager.ui",
@@ -6,6 +5,7 @@ files = {
     "mainview": "ui/main_window.ui",
     "settingsview": "ui/settings_window.ui",
     "projectsettings_view": "ui/projectsettings_window.ui",
+    "export_view": "ui/export.ui",
     "timeline_scrollarea_view": "ui/timeline_scroll_area.ui",
     "timeline_view": "ui/timeline_view.ui"
 }
@@ -21,6 +21,7 @@ strings = {
     "de": "strings/de/strings.xml",
     "en": "strings/en/strings.xml"
 }
+
 
 class Resources:
     """
