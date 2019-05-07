910edb8939ca1724e922a114eb2ac25c8b7e6a3e
Felix Dittrich <felixdittrich92@gmail.com>
2019-05-07 15:46:08 +0200

    #37 check color automation

--- code/src/main/python/model/data/presentation.py
+++ code/src/main/python/model/data/presentation.py
@@ -21,7 +21,7 @@ class Presentation:
         self.files = []
 
     
-    def convert_pdf(self, folder_path, folder_name):
+    def convert_pdf(self, folder_path, folder_name, resolution):
 
         """
         a method that takes a path and a PDF file, converts them to JPG, and then saves the individual images
@@ -29,6 +29,7 @@ class Presentation:
     
         @param folder_path: path to the project folder
         @param folder_name: name of the project folder
+        @param resolution: resolution for every converted pdf picture
         """
 
         input_file = Path(self.file_path, self.filename)
@@ -36,7 +37,7 @@ class Presentation:
         if check_pdf == True:
             folder = Path(folder_path, folder_name)
 
-            pages = convert_from_path(str(input_file), 250)
+            pages = convert_from_path(str(input_file), resolution) #Standardwert sollte 250 sein
 
             for page_number, page in enumerate(pages, start=1):
                 target = folder / f"{page_number:03d}.jpg"
@@ -49,23 +50,27 @@ class Presentation:
             print("the datatype must be .pdf")
 
 
-    def check_color(self, y1, y2, x1, x2):
+    def check_color(self):
         """
         a method which checks if the place for a video is free to show it 
-
-        @param y1: Point(x,min) in a coordinate system for the region of interest
-        @param y2: Point(x,max)
-        @param x1: Point(min, y)
-        @param x2: Point(max, y)
 
         @return: True if region of interest is completly white or gray
         """
         input_file = Path(self.file_path, self.filename)
-        # hier berechnen prozentual
+        picture = cv2.imread(str(input_file))
+        height = picture.shape[0]
+        width = picture.shape[1]
+        # upper y point
+        y1 = int((73.8 * height) / 100)
+        # lower y point
+        y2 = int((94.7 * height) / 100)
+        # left x point
+        x1 = int((79.3 * width) / 100)
+        # right x point
+        x2 = int(width)
         white = 255
         gray = 32
         img = cv2.imread(str(input_file), cv2.IMREAD_GRAYSCALE)
-        # automatisieren img width and height y1/y2 x1/x2 prozentual berechnen
         roi = img[y1:y2, x1:x2]
 
         if np.all(roi == white) == True:
@@ -75,16 +80,12 @@ class Presentation:
         else:
             return False
 
-    def picture_in_presentation(self, file_path_small_img, small_img, y1, y2, x1, x2):
+    def picture_in_presentation(self, file_path_small_img, small_img):
         """
         a method which takes two images and overlay the second one above the first one if place is white
 
         @param file_path_small_img: the path to the overlay image
         @param small_img: the name of the overlay image
-        @param y1: Point(x,min) in a coordinate system for the region of interest
-        @param y2: Point(x,max)
-        @param x1: Point(min, y)
-        @param x2: Point(max, y)
         """
 
         large_img = Path(self.file_path, self.filename)
@@ -94,12 +95,13 @@ class Presentation:
 
         small_img = Path(file_path_small_img, small_img)
         small_img = cv2.imread(str(small_img))
-        small_img = cv2.resize(small_img, (250, 200)) #automatisieren ?
+        small_img = cv2.resize(small_img, (250, 200)) # little picture size (width,height)
 
-        x_offset = width - 250 #only for resolution 250 
-        y_offset = height - 235 #only for resolution 250 
+        x_offset = width - 250 # only for resolution 250 
+        bottom = ((3.7 * height) / 100) # blue bottom ground
+        y_offset = int((height - 200) - bottom) # only for resolution 200 
 
-        if self.check_color(y1, y2, x1, x2) == True: 
+        if self.check_color() == True: 
             large_img[y_offset:y_offset+small_img.shape[0], x_offset:x_offset+small_img.shape[1]] = small_img
             self.files.append(Slide(large_img[y_offset:y_offset+small_img.shape[0], x_offset:x_offset+small_img.shape[1]]))
         else:
