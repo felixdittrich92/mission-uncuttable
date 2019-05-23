b5d2c1662edbc6dd0f691fb480288858e99b951b
Felix <you@example.com>
2019-05-23 13:00:26 +0200

    auto

--- code/src/main/python/controller/autocut_controller.py
+++ code/src/main/python/controller/autocut_controller.py
@@ -3,3 +3,26 @@ class AutocutController:
     def __init__(self, view):
         self.autocut_view = view
         self.autocut_view.show()
+
+    def pickVideoFile(self):
+        supported_filetypes = Settings.get_instance().get_dict_settings()["AutoCutVideo"]["import_formats"]
+        fileName, _ = QFileDialog.getOpenFileName(
+            self,
+            'QFileDialog.getOpenFileNames()',
+            '',
+            (
+                supported_filetypes
+            )
+        )
+    
+    def pickPdfFile(self):
+        supported_filetypes = Settings.get_instance().get_dict_settings()["AutoCutPDF"]["import_formats"]
+        fileName, _ = QFileDialog.getOpenFileName(
+            self,
+            'QFileDialog.getOpenFileNames()',
+            '',
+            (
+                supported_filetypes
+            )
+        )
+
