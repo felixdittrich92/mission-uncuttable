5ffa3d7c7d14afcf69383ee2e8112173634ff57f
Clemens Zwinzscher <clemens.zwinzscher@stud.htwk-leipzig.de>
2019-06-12 18:26:27 +0200

    remove unused code from language class

--- code/src/main/python/config/language.py
+++ code/src/main/python/config/language.py
@@ -4,28 +4,17 @@ from .resources import Resources
 
 class Language:
     current = None
-    __current = None
     languages = []
 
     def __init__(self, language=None):
         for name, path in dict(Resources.strings).items():
             Language.languages.append(name)
             setattr(Language, name, objectify.fromstring(etree.tostring(etree.parse(path))))
-            setattr(Language, "__" + name, etree.parse(path).getroot())
         if language is None:
             language = "de"
             print("No language")
 
         Language.current = getattr(Language, language)
-        Language.__current = getattr(Language, "__" + language)
-
-    @staticmethod
-    def sub(text):
-        sub = Language.__current.findtext(text)
-        if sub is None:
-            return text
-        else:
-            return sub
 
     @staticmethod
     def set_language(lang):
