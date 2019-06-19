0ed33deca83d9c3022cbe645b26c0834c124958e
Clemens Zwinzscher <clemens.zwinzscher@stud.htwk-leipzig.de>
2019-06-19 18:06:52 +0200

    implement remove function correctly

--- code/src/main/python/model/data/timeable_group.py
+++ code/src/main/python/model/data/timeable_group.py
@@ -12,8 +12,8 @@ class TimeableGroup:
 
     def remove_timeable(self, id):
         """ Removes a timeable from the group """
-        if id not in self.ids:
-            return
+        while id in self.ids:
+            self.remove(id)
 
     def is_move_possible(self, pos):
         """ checks if all timeables in the group can be moved to pos """
