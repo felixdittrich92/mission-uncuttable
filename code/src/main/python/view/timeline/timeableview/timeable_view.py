f98eb6472fc65dcce46a70ec94de243d646e2600
Clemens Zwinzscher <clemens.zwinzscher@stud.htwk-leipzig.de>
2019-07-03 19:18:26 +0200

    add option to cut timeable at timeneedle position

--- code/src/main/python/view/timeline/timeableview/timeable_view.py
+++ code/src/main/python/view/timeline/timeableview/timeable_view.py
@@ -1,7 +1,8 @@
 from PyQt5.QtCore import (QPoint, QRectF, QByteArray, QDataStream, QIODevice,
                           QMimeData, Qt, QSize, pyqtSignal)
-from PyQt5.QtGui import QBrush, QColor, QDrag
-from PyQt5.QtWidgets import QMenu, QAction, QApplication, QGraphicsItem, QGraphicsRectItem
+from PyQt5.QtGui import QBrush, QColor, QDrag, QTransform
+from PyQt5.QtWidgets import (QMenu, QAction, QApplication, QGraphicsItem,
+                             QGraphicsRectItem, QWidget)
 
 from controller import TimelineController
 from model.data import FileType
@@ -132,7 +133,8 @@ class TimeableView(QGraphicsRectItem):
 
         px = get_pixmap_from_file(self.model.file_name, frame)
         if px is not None:
-            self.pixmap = px.scaled(QSize(100, self.height - 4.0), Qt.KeepAspectRatio, transformMode = 1)
+            self.pixmap = px.scaled(QSize(100, self.height - 4.0), Qt.KeepAspectRatio,
+                                    transformMode=1)
         else:
             self.pixmap = None
 
@@ -152,18 +154,21 @@ class TimeableView(QGraphicsRectItem):
         menu = QMenu()
         menu.setStyleSheet(open(Resources.files.qss_dark, "r").read())
 
-
         delete = QAction(str(Language.current.timeable.delete))
         menu.addAction(delete)
         delete.triggered.connect(lambda: self.delete(hist=True))
 
-        cut = QAction(str(Language.current.timeable.cut))
-        menu.addAction(cut)
-        cut.triggered.connect(lambda: self.cut(event.pos().x()))
+        cut_here = QAction(str(Language.current.timeable.cut_here))
+        menu.addAction(cut_here)
+        cut_here.triggered.connect(lambda: self.cut_here(event.pos().x()))
+
+        cut_timeneedle = QAction(str(Language.current.timeable.cut_timeneedle))
+        menu.addAction(cut_timeneedle)
+        cut_timeneedle.triggered.connect(self.cut_timeneedle)
 
         settings = QAction(str(Language.current.timeable.settings))
         menu.addAction(settings)
-        settings.triggered.connect(lambda: self.settings())
+        settings.triggered.connect(self.settings)
 
         if self.group_id is not None:
             remove_from_group = QAction(str(Language.current.timeable.group_remove))
@@ -195,13 +200,31 @@ class TimeableView(QGraphicsRectItem):
         """ Removes the timeableview from the track """
         self.scene().removeItem(self)
 
-    def cut(self, pos):
+    def cut_here(self, pos):
         """
         cuts the timeable in two parts
 
         @param pos: x position on the timeable where it's cut
         """
         if pos < TIMEABLE_MIN_WIDTH and self.width >= 2 * TIMEABLE_MIN_WIDTH:
+            return
+
+        self.__controller.split_timeable(self.view_id, self.resizable_right,
+                                         self.width, self.model.clip.End(), pos)
+
+    def cut_timeneedle(self):
+        """
+        Cuts the Timeable at the position of the Timeneedle, but only if the
+        Timeneedle is on this Timeable.
+        """
+        # get the Timeneedle object
+        needle = self.__controller.get_timelineview().track_frame_frame.findChild(
+            QWidget, "needle_bottom")
+
+        pos = needle.x()
+
+        # do nothing if the Timeneedle is not on this Timeable
+        if not self.scene().itemAt(QPoint(pos, 0), QTransform()) is self:
             return
 
         self.__controller.split_timeable(self.view_id, self.resizable_right,
